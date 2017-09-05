a6cec4da1d75a8f76d5e126933888d0f63e67b79
MitulMistry <MitulMistryDev@gmail.com>
2017-09-05 17:33:49 -0500

    Organize CLI methods

--- src/CLI.java
+++ src/CLI.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class CLI {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //run program
+		CLI cli = new CLI();
+		cli.run();
+	}
+	
+	public void run() {
 		Scanner scanner = new Scanner(System.in); //for input
 		int input;
 		
@@ -14,13 +19,26 @@ public class CLI {
 			System.out.println("2. Exit");
 			
 			input = scanner.nextInt();
-			System.out.println(input);
+			this.processInput(input);
 		} while (input != 2);
+		
+		if (scanner != null) { //close scanner
+			scanner.close();
+		}
 		
 		System.exit(0);
 	}
 	
-	private void processInput(input) {
-		
+	private void processInput(int input) {
+		switch (input) {
+			case 1: System.out.println("Generating world...");
+				break;
+			
+			case 2: System.out.println("Goodbye");
+				break;
+				
+			default: System.out.println("Invalid");
+				break;
+		}
 	}
 }
